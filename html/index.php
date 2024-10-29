4bae11706190d513203d675e9bf0cd185d6e7a96
ponponumi <ponponumi@gmail.com>
2024-10-29 17:03:58 +0900

    テスト用のコードを追加

--- html/index.php
+++ html/index.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once __DIR__ . "/../vendor/autoload.php";
+
+use Ponponumi\HtmlAttributeCreate\Create;
+
+var_dump(Create::raw("#123hello.hello-world.Hello_World#hello,helloWorld123"));
