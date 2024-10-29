77969098473b6cd058ef56b6a892df73a97bce96
ponponumi <ponponumi@gmail.com>
2024-10-29 17:50:39 +0900

    HTML属性として取得するテストを追加

--- html/index.php
+++ html/index.php
@@ -6,3 +6,11 @@ use Ponponumi\HtmlAttributeCreate\Create;
 
 var_dump(Create::raw("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello"));
 var_dump(Create::htmlData("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello"));
+var_dump(Create::htmlAttribute("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello"));
+var_dump(Create::htmlAttribute("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello",0));
+var_dump(Create::htmlAttribute("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello",1));
+var_dump(Create::htmlAttribute("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello",2));
+var_dump(Create::htmlAttribute("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello",3));
+var_dump(Create::htmlAttribute("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello",0,1));
+var_dump(Create::htmlAttribute("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello",0,2));
+var_dump(Create::htmlAttribute("#123hello.hello-world.Hello_World#hello,helloWorld123.123Hello",0,3));
