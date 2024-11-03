beb8bf94e0ccabc21d0facf889fcd19424aa7a24
ponponumi <ponponumi@gmail.com>
2024-11-03 15:58:01 +0900

    バグが起こらない事を確認

--- html/empty_delete_check.php
+++ html/empty_delete_check.php
@@ -6,3 +6,7 @@ use Ponponumi\HtmlAttributeCreate\Create;
 
 var_dump(Create::htmlAttribute(".hello-world"));
 var_dump(Create::htmlAttribute("#hello-world"));
+var_dump(Create::htmlAttribute(".hello-world",0,1));
+var_dump(Create::htmlAttribute("#hello-world",0,1));
+var_dump(Create::htmlAttribute(".hello-world",0,2));
+var_dump(Create::htmlAttribute("#hello-world",0,2));
